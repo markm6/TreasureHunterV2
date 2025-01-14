d1db875b06a7ce7a451233c48e4ebd16b0ee815f
BT_1N3_19 <sample@bths.edu>
2025-01-14 10:24:52 -0500

    Game Over

--- src/Hunter.java
+++ src/Hunter.java
@@ -27,6 +27,10 @@ public class Hunter {
         return hunterName;
     }
 
+    public int getGold() {
+        return gold;
+    }
+
     /**
      * Updates the amount of gold the hunter has.
      *
@@ -34,9 +38,6 @@ public class Hunter {
      */
     public void changeGold(int modifier) {
         gold += modifier;
-        if (gold < 0) {
-            gold = 0;
-        }
     }
 
     /**
